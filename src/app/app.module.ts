ddbf2bc8bd2e1eb7d41ac6a8f95f7f07ddde40de
ortega-daniel <ic.danielortega@gmail.com>
2022-06-03 11:43:15 -0700

    implemented basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" },
+    ])
   ],
   bootstrap: [AppComponent]
 })
